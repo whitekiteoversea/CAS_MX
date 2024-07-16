cde6294e829f6f20a338de1ea7ef553b26ddb60a
whitekiteoversea <754641132@qq.com>
2024-07-16 13:12:01 +0800

    FIX: 调整电机默认转动方向为向下，配合上位机逻辑

--- USER/userApp.h
+++ USER/userApp.h
@@ -57,7 +57,7 @@
 #define HAL_SDRAM_ENABLE       			     (1)
 #define HAL_SDRAM_TEST_ENABLE                (0)
 #define HAL_DAC_ENABLE           			 (0)
-#define HAL_EEPROM_ENABLE        			 (1)
+#define HAL_EEPROM_ENABLE        			 (0)
 #define HAL_LCD_ENABLE                       (0)  // CANOpen与LCD相冲
 
 extern uint8_t gDATABUF[DATA_BUF_SIZE];  
